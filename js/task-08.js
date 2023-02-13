0af3d84a627647248bc0e96033997eb1d0d7966c
filipchukdv <filipchukdv@gmail.com>
2023-02-13 12:53:12 +0200

    рефакторинг 8 завдання

--- js/task-08.js
+++ js/task-08.js
@@ -3,14 +3,17 @@ const formRef = document.querySelector('form.login-form');
 
 const onFormSubmit = e => {
   e.preventDefault();
-  const formElements = e.currentTarget.elements;
-  if (!(formElements.email.value && formElements.password.value)) {
+  const email = e.currentTarget.elements.email;
+  const password = e.currentTarget.elements.password;
+
+  if (!(email.value &&  password.value)) {
     alert('Увага, всі поля повинні бути заповнен');
     return;
   }
+  
   const result = {};
-  result[formElements.email.name] = formElements.email.value;
-  result[formElements.password.name] = formElements.password.value;
+  result[email.name] = email.value;
+  result[password.name] = password.value;
   console.log(result);
   e.currentTarget.reset();
 };
